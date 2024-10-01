804c77e4453be9fafeea6e16821e6d5d5ac1c39a
EthanRule <ethan2002@live.com>
2024-10-01 14:58:07 -0700

    Cell class complete, spreadsheet class mid complete

---
+++ Solution/SpreadsheetEngine/Cell.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace SpreadsheetEngine
+{
+    public abstract class Cell : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected readonly int rowIndex;
+        protected readonly int columnIndex;
+        protected string text;
+        protected string value;
+
+        public Cell(int rowIndex, int columnIndex)
+        {
+            this.rowIndex = rowIndex;
+            this.columnIndex = columnIndex;
+            this.text = string.Empty;
+            this.value = string.Empty;
+        }
+
+        public int RowIndex
+        {
+            get { return this.rowIndex; }
+        }
+
+        public int ColumnIndex
+        {
+            get { return this.columnIndex; }
+        }
+
+        public string Text
+        {
+            get { return this.text; }
+
+            set
+            {
+                if (value == Text) { return; }
+                this.text = value;
+                {
+                    // Fire Property Changed Event. ?.Invoke used to prevent potential null ref
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Text"));
+                }
+            }
+        }
+
+        public string Value
+        {
+            get
+            {
+                if (this.value != null && this.value[0] == '=')
+                {
+                    // evaluate the expression and return the value
+                    double expressionResult = 3.14;
+
+                    return expressionResult.ToString();
+                }
+                return this.text;
+            }
+
+            protected set // ensure only the Spreadsheet Class can set this, but no others can.
+            {
+                if (value == this.value) { return; }
+                this.value = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Value"));
+            }
+        }
+    }
+}
---
+++ Solution/SpreadsheetEngine/Spreadsheet.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpreadsheetEngine
+{
+    internal class Spreadsheet
+    {
+        private Cell[,] cellMatrix;
+
+        public event PropertyChangedEventHandler CellPropertyChanged;
+
+        public Spreadsheet(int rows, int columns)
+        {
+            this.cellMatrix = new Cell[rows, columns];
+
+            // subscribe to cells property changed
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    this.cellMatrix[i, j].PropertyChanged += OnCell
+                }
+            }
+
+        }
+    }
+}
